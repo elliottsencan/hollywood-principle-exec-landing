4d909124254aba1c1eb452d8366ac2d974da5d9d
elliott.sencan <elliott.sencan@servicenow.com>
2019-02-23 13:25:38 -0800

    lightning fast first pass

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,15 +8,15 @@ const Header = (props) => (
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>Exec</h1>
+                <p>A digital record executive to help maximize your releases<br />
+                by <a href="https://records.hollywoodprinciple.com">Hollywood Principle Records</a></p>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
+                {/* <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li> */}
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
             </ul>
--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 import pic01 from '../images/pic01.jpg'
 import pic02 from '../images/pic02.jpg'
-import pic03 from '../images/pic03.jpg'
+import pic03 from '../images/about.jpg'
 
 class Main extends React.Component {
   render() {
@@ -32,30 +32,33 @@ class Main extends React.Component {
         <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">About</h2>
           <span className="image main"><img src={pic03} alt="" /></span>
-          <p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
+          <p>The music industry is failing. Unless you are Drake, Cardi B, Ed Sheerhan, Billie Eilish? Forget about making a living with music. Who has heard this narrative before? Who thought this when someone said “I want to be a musician”. Show of hands, real quick. I’ve thought it. This tale of doom and gloom has been parroted throughout society for almost two decades, all as we live through a massive revival of the Music Industry. Artists, labels and publishers are cashing in on the growing popularity of streaming platforms like Spotify and Apple music. There had never be an independent musician. So why does this narrative persist? Why do we think of independent artist as lyft drivers with a hobby? we believe it’s on key reason: the skills required to write amazing music are completely different than the skill required to monetize amazing music. </p>
+          <p>In the past, the answer to this is simple: sign with a label. Their business is the music business after all. But it’s a people intensive business with gatekeepers that doesn’t scale, and it can’t cope with the deluge of new music being produced. As a result, labels lean on popularity as a key metric to decide who to sign, creating a giant big catch-22: youre act must be popular to get signed, and to be popular you need to get signed. So how does an artist get from 0-1?</p>
+          <p>This is where Exec by Hollywood Principle Records comes in: a digital label exec to help artists. Our labels’s innovative digital services model has a simple pitch: Give us good content, we will provide artists with a snapshot of their current progress and how artists how to maximize it, the same way we do for our current roster artists. We offer a tailored comprehensive plan generated for each of an artists releases that *tells an artist what to do to be as successful as possible*, all for a low monthly fee. Tell us your type of release and your budget, no matter how big or how small, and we will dynamically generate the right plan for you, while automating as much of the dirty work as possible, and giving you easy to digest reports on how effective your efforts have been.</p>
+          <p>Releasing an album? Our software will tell you if/when you should run Spotify ads. It will tell you which blogs to reach out to, and when, as well as templates of what to say. It will tell you when to run instagram ads, at what audience. Which agencies to register your songs with, ensuring you get paid, and much more. Its constantly learning what works from all our campaigns, becoming refined, more effective, and more automated to make the hardest part of being an independent musician straightforward, simple, and efficient.</p>
           {close}
         </article>
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
-            <div className="field half first">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div className="field half">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
-            </div>
-            <div className="field">
+          <form action="https://hollywoodprinciple.us9.list-manage.com/subscribe/post?u=61143e36dd2b444f1dd45ad6b&amp;id=55c9d07ae7" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+          <div className="field half first">
+              <label htmlFor="mce-NAME">Name</label>
+              <input type="text"  name="NAME" class="" id="mce-NAME" />
+          </div>
+          <div className="field half">
+              <label htmlFor="mce-EMAIL">Email</label>
+              <input type="email"  name="EMAIL" class="required email" id="mce-EMAIL"/>
+          </div>
+          <div className="field">
               <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
-            </div>
-            <ul className="actions">
+              <textarea rows="6"  name="MESSAGE" class="required" id="mce-MESSAGE"></textarea>
+          </div>
+          <ul className="actions">
               <li><input type="submit" value="Send Message" className="special" /></li>
-              <li><input type="reset" value="Reset" /></li>
-            </ul>
-          </form>
+              <li><input type="reset" value="Clear" /></li>
+          </ul>
+      </form>
           <ul className="icons">
             <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
             <li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
